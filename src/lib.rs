57cea371101cbaff4115ef56f0ee82c66a8a765f
Lopes <Sergio.Lopes@netzsch.com>
2025-05-07 10:53:57 +0200

    Initial

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+// Provide git blame
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use git2::{BlameOptions, Repository};
+
+    #[test]
+    fn it_works() {
+        let repo = Repository::open(".").expect("hi");
+
+        let mut cwd = PathBuf::new();
+        cwd.push("file.txt");
+
+        println!("{}", cwd.to_string_lossy());
+
+        assert!(cwd.exists());
+        let blame = repo.blame_file(cwd.as_path(), None).expect("hi");
+
+        for line in 0..blame.len() {
+            println!("{:#?}", blame.get_line(line).expect("hi").lines_in_hunk());
+        }
+    }
+}
